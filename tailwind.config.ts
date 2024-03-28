f32031e812bb45490be32db046e2e36c7b73f499
blazingh <hadibaalbaki@skiff.com>
2024-03-28 22:31:09 +0300

    fixed global font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,7 @@ const config = {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
+        poppins: ["var(--font-sans)", ...fontFamily.sans],
       },
       colors: {
         border: "hsl(var(--border))",

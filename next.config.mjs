4640ce952c38f086001f98cf1df4aedaba63d3cc
blazingh <hadibaalbaki@skiff.com>
2024-04-01 10:18:31 +0300

    added new packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['three'],
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
     return config;

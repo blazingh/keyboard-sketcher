e35ab850d75477307d8bcd416c31b2ae3080ca23
blazingh <hadibaalbaki@skiff.com>
2024-04-06 10:03:49 +0300

    moved outline node to the constant file

--- src/components/sketcher/sketcher-workspace.tsx
+++ src/components/sketcher/sketcher-workspace.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { cn } from '@/lib/utils';
 import { PlusIcon } from 'lucide-react';
-import { useEffect, useMemo, useRef } from 'react';
+import { useMemo } from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
 import ReactFlow, {
   Background,
   BackgroundVariant,
-  Controls,
   NodeChange,
   NodeToolbar,
   Position,
@@ -85,26 +84,6 @@ export function SketcherWorkSpace() {
   useHotkeys(Key.ArrowDown, () => handleMoveNode('ArrowDown'))
   useHotkeys(Key.ArrowLeft, () => handleMoveNode('ArrowLeft'))
   useHotkeys(Key.ArrowRight, () => handleMoveNode('ArrowRight'))
-
-
-  useEffect(() => {
-    const changes: NodeChange[] = []
-    changes.push({
-      type: 'add',
-      item: {
-        id: "0",
-        type: 'outline',
-        position: { x: 0, y: 0 },
-        data: { label: 'outline' },
-        zIndex: 0,
-        width: 5000,
-        height: 5000,
-        selectable: false,
-        draggable: false,
-      },
-    })
-    onNodesChange(changes)
-  }, [])
 
   return (
     <div className='relative w-svw h-svh'>
--- src/constants/temp.tsx
+++ src/constants/temp.tsx
@@ -1,6 +1,17 @@
 import { Node } from "reactflow";
 
 export const initialNodes: Node[] = [
+  {
+    id: "0",
+    type: 'outline',
+    position: { x: 0, y: 0 },
+    data: { label: 'outline' },
+    zIndex: 0,
+    width: 5000,
+    height: 5000,
+    selectable: false,
+    draggable: false,
+  },
   { id: "1", type: 'mcu', position: { x: 190, y: 85 }, data: { label: 'mcu', rotation: '0', width: 210, height: 520 }, zIndex: 0 },
   { id: "2", type: 'switch', position: { x: 0, y: 0 }, data: { label: 'Switch', rotation: '0', width: 140, height: 140 }, zIndex: 10 },
   { id: "3", type: 'switch', position: { x: 190, y: 0 }, data: { label: 'Switch', rotation: '0', width: 140, height: 140 }, zIndex: 10 },

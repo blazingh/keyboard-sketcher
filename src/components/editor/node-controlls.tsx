a862d5344adf9cdec82b22fb491d902ecafa5858
blazingh <hadibaalbaki@skiff.com>
2024-04-07 23:07:20 +0300

    updated node controller

--- src/components/editor/node-controlls.tsx
+++ src/components/editor/node-controlls.tsx
@@ -1,11 +1,12 @@
 "use client"
+import Draggable from 'react-draggable';
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import { EditorContext } from "@/contexts/editor-context"
 import { workSpaceContext } from "@/contexts/workspace-context"
 import { calculateCenterPosition } from "@/lib/positions"
 import { cn } from "@/lib/utils"
-import { ArrowUp, ArrowRight, ArrowDown, ArrowLeft, PencilRuler, RotateCcw, RotateCw, MoveHorizontal, MoveVertical, RefreshCw } from "lucide-react"
+import { ArrowUp, ArrowRight, ArrowDown, ArrowLeft, PencilRuler, RotateCcw, RotateCw, MoveHorizontal, MoveVertical, RefreshCw, Hand, GripHorizontal } from "lucide-react"
 import { useContext, useEffect, useMemo, useState } from "react"
 
 
@@ -56,77 +57,83 @@ export default function NodesControll() {
   if (!visible) return null
 
   return (
-    <div
-      className={cn(
-        "absolute bottom-0 right-0 z-10 w-[160px] h-[160px]",
-        "bg-background p-4 rounded-lg border",
-        "flex flex-col gap-2",
-        "-translate-x-[1rem] -translate-y-[1rem]"
-      )}
+    <Draggable
+      handle='.handle'
     >
-      <div className="col-span-3 flex items-end justify-between w-full gap-2">
-        <span className="flex gap-1 line-clamp-1 whitespace-nowrap">
-          {diffPos.x > 0 && <ArrowRight className="w-4" />}
-          {diffPos.x < 0 && <ArrowLeft className="w-4" />}
-          {diffPos.x === 0 && <MoveHorizontal className="w-4" />}
-          {`: ${diffPos.x / 10}`}
-        </span>
-        <span className="text-right flex gap-1 line-clamp-1 whitespace-nowrap font-medium">
-          {diffPos.y > 0 && <ArrowDown className="w-4" />}
-          {diffPos.y < 0 && <ArrowUp className="w-4" />}
-          {diffPos.y === 0 && <MoveVertical className="w-4" />}
-          {`: ${diffPos.y / -10}`}
-        </span>
+      <div
+        className={cn(
+          "absolute bottom-5 right-5 z-10 w-[160px] h-[160px]",
+          "bg-background p-4 rounded-lg border",
+          "flex flex-col gap-2",
+        )}
+      >
+        <div className='handle absolute -top-2 left-1/2 -translate-x-1/2 bg-secondary rounded px-1 opacity-50 hover:opacity-100'>
+          <GripHorizontal className='w-4 h-4' />
+        </div>
+        <div className="col-span-3 flex items-end justify-between w-full gap-2">
+          <span className="flex gap-1 line-clamp-1 whitespace-nowrap">
+            {diffPos.x > 0 && <ArrowRight className="w-4" />}
+            {diffPos.x < 0 && <ArrowLeft className="w-4" />}
+            {diffPos.x === 0 && <MoveHorizontal className="w-4" />}
+            {`: ${diffPos.x / 10}`}
+          </span>
+          <span className="text-right flex gap-1 line-clamp-1 whitespace-nowrap font-medium">
+            {diffPos.y > 0 && <ArrowDown className="w-4" />}
+            {diffPos.y < 0 && <ArrowUp className="w-4" />}
+            {diffPos.y === 0 && <MoveVertical className="w-4" />}
+            {`: ${diffPos.y / -10}`}
+          </span>
+        </div>
+        <div className="grid grid-cols-3 gap-2 *:w-10 *:h-10 *:flex-shrink-0 w-fit">
+          <div />
+          <Button
+            onClick={() => editor.moveSelectedNodes("U")}
+            onMouseDown={() => handleMouseDown("U")}
+            onTouchStart={() => handleMouseDown("U")}
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseUp}
+            onTouchEnd={handleMouseUp}
+            onTouchCancel={handleMouseUp}
+          >
+            <ArrowUp className="flex-shrink-0 w-5" />
+          </Button>
+          <div />
+          <Button
+            onClick={() => editor.moveSelectedNodes("L")}
+            onMouseDown={() => handleMouseDown("L")}
+            onTouchStart={() => handleMouseDown("L")}
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseUp}
+            onTouchEnd={handleMouseUp}
+            onTouchCancel={handleMouseUp}
+          >
+            <ArrowLeft className="flex-shrink-0 w-5" />
+          </Button>
+          <Button
+            onClick={() => editor.moveSelectedNodes("D")}
+            onMouseDown={() => handleMouseDown("D")}
+            onTouchStart={() => handleMouseDown("D")}
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseUp}
+            onTouchEnd={handleMouseUp}
+            onTouchCancel={handleMouseUp}
+          >
+            <ArrowDown className="flex-shrink-0 w-5" />
+          </Button>
+          <Button
+            onClick={() => editor.moveSelectedNodes("R")}
+            onMouseDown={() => handleMouseDown("R")}
+            onTouchStart={() => handleMouseDown("R")}
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseUp}
+            onTouchEnd={handleMouseUp}
+            onTouchCancel={handleMouseUp}
+          >
+            <ArrowRight className="flex-shrink-0 w-5" />
+          </Button>
+        </div>
       </div>
-      <div className="grid grid-cols-3 gap-2 *:w-10 *:h-10 *:flex-shrink-0 w-fit">
-        <div />
-        <Button
-          onClick={() => editor.moveSelectedNodes("U")}
-          onMouseDown={() => handleMouseDown("U")}
-          onTouchStart={() => handleMouseDown("U")}
-          onMouseUp={handleMouseUp}
-          onMouseLeave={handleMouseUp}
-          onTouchEnd={handleMouseUp}
-          onTouchCancel={handleMouseUp}
-        >
-          <ArrowUp className="flex-shrink-0 w-5" />
-        </Button>
-        <div />
-        <Button
-          onClick={() => editor.moveSelectedNodes("L")}
-          onMouseDown={() => handleMouseDown("L")}
-          onTouchStart={() => handleMouseDown("L")}
-          onMouseUp={handleMouseUp}
-          onMouseLeave={handleMouseUp}
-          onTouchEnd={handleMouseUp}
-          onTouchCancel={handleMouseUp}
-        >
-          <ArrowLeft className="flex-shrink-0 w-5" />
-        </Button>
-        <Button
-          onClick={() => editor.moveSelectedNodes("D")}
-          onMouseDown={() => handleMouseDown("D")}
-          onTouchStart={() => handleMouseDown("D")}
-          onMouseUp={handleMouseUp}
-          onMouseLeave={handleMouseUp}
-          onTouchEnd={handleMouseUp}
-          onTouchCancel={handleMouseUp}
-        >
-          <ArrowDown className="flex-shrink-0 w-5" />
-        </Button>
-        <Button
-          onClick={() => editor.moveSelectedNodes("R")}
-          onMouseDown={() => handleMouseDown("R")}
-          onTouchStart={() => handleMouseDown("R")}
-          onMouseUp={handleMouseUp}
-          onMouseLeave={handleMouseUp}
-          onTouchEnd={handleMouseUp}
-          onTouchCancel={handleMouseUp}
-        >
-          <ArrowRight className="flex-shrink-0 w-5" />
-        </Button>
-      </div>
-    </div>
+    </Draggable>
   )
 }
 

cd7a73f3aa39d19dbebb92ad027b57f062ea5a0c
blazingh <hadibaalbaki@skiff.com>
2024-04-13 18:21:41 +0300

    added new animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,12 +75,17 @@ const config = {
         "pop-in": {
           '0%': { scale: "0" },
           '100%': { scale: "1" },
+        },
+        "pop-out": {
+          '0%': { scale: "1" },
+          '100%': { scale: "0" },
         }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "pop-in": "pop-in 0.1s ease-in-out"
+        "pop-in": "pop-in 0.1s ease-in-out",
+        "pop-out": "pop-out 0.1s ease-in-out"
       },
     },
   },

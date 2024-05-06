1c3d88248f8644965b89fc1d059779d671b9d806
blazingh <hadibaalbaki@skiff.com>
2024-05-06 19:07:46 +0300

    updated helper tooltip icon

--- src/components/helper-tooltip.tsx
+++ src/components/helper-tooltip.tsx
@@ -1,11 +1,11 @@
-import { HelpCircle } from "lucide-react";
+import { Info } from "lucide-react";
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent } from "./ui/dropdown-menu";
 
 export function HelperTooltip({ desc }: { desc: string }) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <HelpCircle className="w-5 h-5" />
+        <Info className="w-5 h-5" />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="px-2 max-w-[200px]">
         <span className="text-xs">

acb48605c0d41835ee638dfe851ec03d614c64b5
blazingh <hadibaalbaki@skiff.com>
2024-05-07 16:14:37 +0300

    addde new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme_provider";
 import { cn } from "@/lib/utils";
@@ -12,6 +13,8 @@ const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-sans",
 })
+
+const typoDraft = localFont({ src: '../Typo-Draft.otf', variable: "--font-typo-draft" })
 
 export const metadata: Metadata = {
   title: "Keyboard Sketcher",
@@ -25,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(poppins.variable)}>
+      <body className={cn(poppins.variable, typoDraft.variable)}>
         <SpeedInsights />
         <Analytics />
         <ThemeProvider
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,7 @@ const config = {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
         poppins: ["var(--font-sans)", ...fontFamily.sans],
+        draft: ["var(--font-typo-draft)"],
       },
       colors: {
         border: "hsl(var(--border))",

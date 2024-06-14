5529254d865e7b45473d2f17620eefb438dbc30a
blazingh <hadibaalbaki@skiff.com>
2024-06-14 22:41:24 +0300

    fixed ruler points not rotating

--- src/components/editor/nodes-ruler-points.tsx
+++ src/components/editor/nodes-ruler-points.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "./stores/editor-store";
+import { rotatePoint } from "./lib/Pos-utils";
 
 export default function NodesRulerPoints() {
   const editor = useEditorStore()
@@ -13,18 +14,21 @@ export default function NodesRulerPoints() {
           { key: "top-left", x: node.pos.x - node.size.w / 2, y: node.pos.y - node.size.h / 2 },
           { key: "bottom-right", x: node.pos.x + node.size.w / 2, y: node.pos.y + node.size.h / 2 },
         ];
-        return positions.map(pos => (
-          <circle
-            className={cn(
-              'stroke-default hover:stroke-white fill-white hover:fill-primary cursor-pointer'
-            )}
-            key={node.id + pos.key}
-            cx={pos.x}
-            cy={pos.y}
-            r={7}
-            strokeWidth={2}
-          />
-        ));
+        return positions.map(pos => {
+          const { x, y } = rotatePoint({ ...pos, r: 0 }, node.pos)
+          return (
+            <circle
+              className={cn(
+                'stroke-default hover:stroke-white fill-white hover:fill-primary cursor-pointer'
+              )}
+              key={node.id + pos.key}
+              cx={x}
+              cy={y}
+              r={7}
+              strokeWidth={2}
+            />
+          )
+        });
       })}
     </g>
 

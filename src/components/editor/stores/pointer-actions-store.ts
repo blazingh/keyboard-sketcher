725a19966d0bc6d4f0d5aa09aa50a26154890266
blazingh <hadibaalbaki@skiff.com>
2024-06-19 19:56:32 +0300

    updatde pointer action store state

--- src/components/editor/stores/pointer-actions-store.ts
+++ src/components/editor/stores/pointer-actions-store.ts
@@ -6,23 +6,42 @@ type State = {
 
   selectedMode: "normal" | "selectionBox" | "ruler" | "addition" | "copy" | "arc"
 
+  transformationOptions: {},
+
+  selectionBoxOptions: {},
+
+  rulerOptions: {},
+
+  additionOptions: {},
+
+  copyOptions: {},
+
   arcOptions: {
     sides: ("left" | "right")[]
     switchCount: number
     switchGap: number
     radius: number
-  }
-
+  },
 
 }
 
 type Action = {
   setSelectedMode: (mode: State["selectedMode"]) => void
-  updateArcOptions: (arc: State["arcOptions"]) => void,
+  updateTransformationOptions: (options: State["transformationOptions"]) => void,
+  updateSelectionBoxOptions: (options: State["selectionBoxOptions"]) => void,
+  updateRulerOptions: (options: State["rulerOptions"]) => void,
+  updateAdditionOptions: (options: State["additionOptions"]) => void,
+  updateCopyOptions: (options: State["copyOptions"]) => void,
+  updateArcOptions: (options: State["arcOptions"]) => void,
 }
 
 const initialState: State = {
   selectedMode: "normal",
+  transformationOptions: {},
+  selectionBoxOptions: {},
+  rulerOptions: {},
+  additionOptions: {},
+  copyOptions: {},
   arcOptions: {
     sides: ["right"],
     switchCount: 5,
@@ -37,9 +56,34 @@ export const PointerAcitonStore = create<PointerActionStoreType>((set, get) => (
   setSelectedMode: (mode) => {
     set({ selectedMode: mode })
   },
-  updateArcOptions: (arcOptions) => {
+  updateTransformationOptions: (options) => {
     set(produce((state: State) => {
-      state.arcOptions = arcOptions
+      state.transformationOptions = options
     }))
-  }
+  },
+  updateSelectionBoxOptions: (options) => {
+    set(produce((state: State) => {
+      state.selectionBoxOptions = options
+    }))
+  },
+  updateRulerOptions: (options) => {
+    set(produce((state: State) => {
+      state.rulerOptions = options
+    }))
+  },
+  updateAdditionOptions: (options) => {
+    set(produce((state: State) => {
+      state.additionOptions = options
+    }))
+  },
+  updateCopyOptions: (options) => {
+    set(produce((state: State) => {
+      state.copyOptions = options
+    }))
+  },
+  updateArcOptions: (options) => {
+    set(produce((state: State) => {
+      state.arcOptions = options
+    }))
+  },
 }))

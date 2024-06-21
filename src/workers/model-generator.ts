9f839a0cf349bc528b63574b2a9335773b1be1c4
blazingh <hadibaalbaki@skiff.com>
2024-06-21 15:18:30 +0300

    minor generator code tweeks

--- src/workers/model-generator.ts
+++ src/workers/model-generator.ts
@@ -40,7 +40,7 @@ type WorkerMessageData = {
 type SimpleNode = {
   type: "switch" | "mcu"
   position: { x: number, y: number, r: number }
-  size: { w: number, h: number }
+  size: { w: number, h: number, p: number }
 }
 
 self.onmessage = async (e: MessageEvent<WorkerMessageData>) => {
@@ -64,31 +64,36 @@ self.onmessage = async (e: MessageEvent<WorkerMessageData>) => {
   o.wallRadius = Math.min(o.wallThickness / 2, o.wallRadius)
   o.wallSwitchPadding = Math.max(o.wallSwitchPadding, 3)
 
+  let switch_nodes: SimpleNode[] = []
+  let mcu_nodes: SimpleNode[] = []
+
   // extract the nodes
-  const nodes: SimpleNode[] = e.data.nodes.map((node: Node) => {
-    return {
+  e.data.nodes.forEach((node: Node) => {
+    const simpleNode: SimpleNode = {
       type: node.type,
       position: { x: node.pos.x / 10, y: node.pos.y / 10, r: node.pos.r },
       size: { w: node.size.w / 10, h: node.size.h / 10, p: node.size.p / 10 }
     }
+    if (node.type === "switch")
+      switch_nodes.push(simpleNode)
+    if (node.type === "mcu")
+      mcu_nodes.push(simpleNode)
   })
 
-  const switch_nodes = nodes.filter((node: any) => node.type === "switch")
-  const mcu_nodes = nodes.filter((node: any) => node.type === "mcu")
-
+  const sides: number[][][] = []
   const borderPoints = getNodesOutinePoints(e.data.nodes as any, {
     wallSwitchPadding: o.wallSwitchPadding * 10,
     wallWidth: o.wallThickness * 10
   })
-  const sides: number[][][] = []
 
-  borderPoints.map((point, index) => {
+  borderPoints.forEach((point, index) => {
     if (index == borderPoints.length - 1)
       sides.push([point, borderPoints[0]])
     else
       sides.push([point, borderPoints[index + 1]])
   })
 
+  // 2d representation of the swithc plate
   let base_plate = transforms.scale([0.1, 0.1, 1], geometries.geom2.create(sides as any))
 
   // extrude the plate to a 3d geometry
@@ -97,7 +102,7 @@ self.onmessage = async (e: MessageEvent<WorkerMessageData>) => {
   const case_corners: Geom3[] = []
 
   // generate the sides of the case from the sides of the plate
-  expansions.offset({ delta: tolerance.tight + o.wallThickness / 2 }, base_plate).sides.map((points) => {
+  expansions.offset({ delta: tolerance.tight + o.wallThickness / 2 }, base_plate).sides.forEach((points) => {
     case_corners.push(
       booleans.union(
         //wall
@@ -108,7 +113,7 @@ self.onmessage = async (e: MessageEvent<WorkerMessageData>) => {
           center: [points[0][0], points[0][1], o.wallHeight / 2],
           segments: 30
         }),
-        // wall bottom part for flatness
+        // wall bottom flat part part
         primitives.roundedCylinder({
           radius: o.wallThickness / 2,
           height: o.wallRadius * 2,
@@ -122,7 +127,7 @@ self.onmessage = async (e: MessageEvent<WorkerMessageData>) => {
   case_corners.push(case_corners[0])
 
   const case_standoffs: Geom3[] = []
-  expansions.offset({ delta: tolerance.tight - o.standoffThickness / 2 + o.wallThickness / 2 }, base_plate).sides.map((points) => {
+  expansions.offset({ delta: tolerance.tight - o.standoffThickness / 2 + o.wallThickness / 2 }, base_plate).sides.forEach((points) => {
     case_standoffs.push(
       primitives.cylinderElliptic({
         height: o.standoffThickness,
@@ -149,23 +154,6 @@ self.onmessage = async (e: MessageEvent<WorkerMessageData>) => {
     )
   )
 
-  // switch cutout
-  switch_nodes.map((node) => {
-    base_case_3d = booleans.subtract(
-      base_case_3d,
-      transforms.translate(
-        [node.position.x, node.position.y, o.baseThickness + o.plateHeight - 4.15],
-        transforms.rotateZ(
-          utils.degToRad(node.position.r),
-          primitives.cuboid({
-            size: [node.size.w, node.size.h, 8.30],
-            center: [0, 0, 0]
-          })
-        )
-      )
-    )
-  })
-
   // cut the mcu from the case
   /*
   mcu_nodes.map((node) => {
@@ -186,7 +174,22 @@ self.onmessage = async (e: MessageEvent<WorkerMessageData>) => {
   */
 
   // cut the switches holes and gruves from the plate
-  switch_nodes.map((node) => {
+  switch_nodes.forEach((node) => {
+    // cut switches from case
+    base_case_3d = booleans.subtract(
+      base_case_3d,
+      transforms.translate(
+        [node.position.x, node.position.y, o.baseThickness + o.plateHeight - 4.15],
+        transforms.rotateZ(
+          utils.degToRad(node.position.r),
+          primitives.cuboid({
+            size: [node.size.w, node.size.h, 8.30],
+            center: [0, 0, 0]
+          })
+        )
+      )
+    )
+    // cut switches from plate
     base_plate_3d = booleans.subtract(
       base_plate_3d,
       // switch cutout
@@ -237,6 +240,7 @@ self.onmessage = async (e: MessageEvent<WorkerMessageData>) => {
     geoms,
     id: e.data.id
   });
+
 };
 
 export { };

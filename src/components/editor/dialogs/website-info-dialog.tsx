8aa5e23f77bebf77a7f0a8fbc61a85634b36f04a
blazingh <hadibaalbaki@skiff.com>
2024-06-23 19:11:39 +0300

    updated website info dialog

--- src/components/editor/dialogs/website-info-dialog.tsx
+++ src/components/editor/dialogs/website-info-dialog.tsx
@@ -1,6 +1,6 @@
 "use Client"
-import { Button, Modal, ModalBody, ModalContent, ModalHeader, ModalProps } from "@nextui-org/react"
-import { X } from "lucide-react"
+import { Button, Divider, Modal, ModalBody, ModalContent, ModalHeader, ModalProps } from "@nextui-org/react"
+import { Github, X } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -32,21 +32,31 @@ export default function WebsiteInfoDialog({ isOpen, onOpenChange }: { isOpen: bo
 
             <ModalBody >
               <div className="mb-2 flex flex-col gap-4 items-center">
-                <p>I am too lazy to write anything</p>
-                <p>Here is my email if you want anything</p>
-                <a href="mailto:hey@hadibaalbaki.com" target="_blank" className="text-primary underline">hey@hadibaalbaki.com</a>
-                <p></p>
-                <p></p>
-                <p className="text-center">This link is only if you managed to capture an elf or grow a money tree</p>
-                <Link href={"https://ko-fi.com/O5O4TP3U4"} target="_blank">
-                  <Image
-                    src={"https://storage.ko-fi.com/cdn/kofi2.png?v=3"}
-                    width={150}
-                    height={50}
-                    alt="Buy me a Coffee at ko-fi.com"
-                  />
-                </Link>
-                <p></p>
+                <div className="flex flex-col items-center">
+                  <p>I am too lazy to write anything</p>
+                  <p>Here is my email if you want anything</p>
+                  <a href="mailto:hey@hadibaalbaki.com" target="_blank" className="text-primary underline">hey@hadibaalbaki.com</a>
+                </div>
+                <Divider />
+                <div className="flex flex-col items-center">
+                  <p >⭐ Star ⭐ the project on Github </p>
+                  <p>or steel the code if you care enough</p>
+                  <Link href={"https://ko-fi.com/O5O4TP3U4"} target="_blank" className="bg-white rounded-xl p-2 mt-2">
+                    <Github className="text-secondary" />
+                  </Link>
+                </div>
+                <Divider />
+                <div className="flex flex-col items-center gap-2">
+                  <p className="text-center">This link is only if you managed to capture an elf or grow a money tree</p>
+                  <Link href={"https://ko-fi.com/O5O4TP3U4"} target="_blank">
+                    <Image
+                      src={"https://storage.ko-fi.com/cdn/kofi2.png?v=3"}
+                      width={150}
+                      height={50}
+                      alt="Buy me a Coffee at ko-fi.com"
+                    />
+                  </Link>
+                </div>
               </div>
             </ModalBody>
 
